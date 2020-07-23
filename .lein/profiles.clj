630b5426e94277e763fa9b0dc5375fe68ee1c00c
straux <straux@users.noreply.github.com>
2020-07-23 11:51:41 +0200

    Update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,18 +1,4 @@
-{:user {:deploy-repositories [["releases"  {:url "http://artifactory.oscaroad.com/artifactory/libs-release-local"
-                                            :creds :gpg}]
-                              ["snapshots" {:url "http://artifactory.oscaroad.com/artifactory/libs-snapshot-local"
-                                            :creds :gpg}]]
-        :signing {:gpg-key "2BC5AB85"}
-        :mirrors {
-                  "central" {:name "central"
-                              :url "http://artifactory.oscaroad.com/artifactory/remote-repos/"
-                              }
-                  #"clojars" {:name "clojars"
-                              :url "http://artifactory.oscaroad.com/artifactory/remote-repos/"
-                              }
-                    }
-
-        :plugins [[lein-cloverage "1.0.6" :exclusions  [org.clojure/clojure]]
+{:user {:plugins [[lein-cloverage "1.0.6" :exclusions  [org.clojure/clojure]]
                   [lein-pprint "1.1.2" :exclusions  [org.clojure/clojure]]
                   [lein-ancient "0.6.8" :exclusions  [org.clojure/clojure]]
                   [lein-codox "0.9.1" :exclusions  [org.clojure/clojure]]]
